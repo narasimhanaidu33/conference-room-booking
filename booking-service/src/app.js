2bcbc9a35820e2536e12d4abe1f2ec817f0ecd42
Narasimha Naidu <narasimhanaidu12102002@gmail.com>
2025-12-23 12:27:24 +0000

    Revert "Fix SPA routing with nginx fallback"
    This reverts commit 6a9dc5eb1d297862cb3734f3fdabd9221bbf43f5.

--- booking-service/src/app.js
+++ booking-service/src/app.js
@@ -4,15 +4,8 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
-
 app.use(express.json());
-
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected (Booking Service)"))
--- room-service/src/app.js
+++ room-service/src/app.js
@@ -6,12 +6,7 @@ const cors = require("cors");
 const app = express();
 connectDB();
 
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/locations", require("./routes/locationRoutes"));
