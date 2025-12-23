7423e3ae8a859a8fb67110d60540db102d04ed5f
Narasimha Naidu <narasimhanaidu12102002@gmail.com>
2025-12-23 09:53:39 +0000

    Enable CORS for frontend access

--- booking-service/src/app.js
+++ booking-service/src/app.js
@@ -5,7 +5,13 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected (Booking Service)"))
--- room-service/src/app.js
+++ room-service/src/app.js
@@ -6,7 +6,12 @@ const cors = require("cors");
 const app = express();
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 
 app.use("/api/locations", require("./routes/locationRoutes"));
